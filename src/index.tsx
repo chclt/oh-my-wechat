8e5cfeab9edb66d3f3fb58589fcab459d7c0695b
Chclt <realchclt@gmail.com>
2025-08-02 22:24:15 +0800

    refactor: WIP

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/query-client.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import {
-  createMemoryHistory,
-  createRouter,
-  RouterProvider,
-} from "@tanstack/react-router";
+import { RouterProvider } from "@tanstack/react-router";
 import router from "./lib/router";
 
 const rootEl = document.getElementById("root");
