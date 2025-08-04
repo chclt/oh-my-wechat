c67ec6a2989078b635346ee9cd4e1cb3aaea3f2f
Chclt <realchclt@gmail.com>
2025-08-05 01:56:47 +0800

    refactor: WIP

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -11,10 +11,7 @@ export function LoaderIcon(props: IconProps) {
       {...props}
     >
       <title>Loader</title>
-      <g
-        clip-path="url(#paint0_angular_781_22_clip_path)"
-        data-figma-skip-parse="true"
-      >
+      <g clipPath="url(#paint0_angular_781_22_clip_path)">
         <g transform="matrix(0 -0.01 0.01 0 12 12)">
           <foreignObject x="-1000" y="-1000" width="2000" height="2000">
             <div
@@ -31,16 +28,16 @@ export function LoaderIcon(props: IconProps) {
         </g>
       </g>
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6Z"
       />
       <circle cx="12" cy="4" r="2" fill="currentColor" />
       <defs>
         <clipPath id="paint0_angular_781_22_clip_path">
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6Z"
           />
         </clipPath>
