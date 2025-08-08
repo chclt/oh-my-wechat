685994c54f00407761f6728a06f80ae20eafe114
Chclt <realchclt@gmail.com>
2025-08-08 13:20:57 +0800

    refactor: remove biome.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { QueryClientProvider } from "@tanstack/react-query";
+import {QueryClientProvider} from "@tanstack/react-query";
 import queryClient from "@/lib/query-client.ts";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { RouterProvider } from "@tanstack/react-router";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {TanStackRouterDevtools} from "@tanstack/react-router-devtools";
+import {RouterProvider} from "@tanstack/react-router";
 import router from "./lib/router";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
-	const root = ReactDOM.createRoot(rootEl);
-	root.render(
-		<React.StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-				<ReactQueryDevtools buttonPosition="bottom-left" />
-				<TanStackRouterDevtools router={router} />
-			</QueryClientProvider>
-		</React.StrictMode>,
-	);
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+        <ReactQueryDevtools buttonPosition="bottom-left"/>
+        <TanStackRouterDevtools router={router}/>
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
 }
