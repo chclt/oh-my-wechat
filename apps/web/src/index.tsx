6e3bd71e32a68dadd135626fe7febaa4e8b9bc82
Chclt <realchclt@gmail.com>
2025-08-20 01:59:30 +0800

    fix

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -4,14 +4,8 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/query-client.ts";
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import {
-	ReactQueryDevtools,
-	ReactQueryDevtoolsPanel,
-} from "@tanstack/react-query-devtools";
-import {
-	TanStackRouterDevtools,
-	TanStackRouterDevtoolsPanel,
-} from "@tanstack/react-router-devtools";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { RouterProvider } from "@tanstack/react-router";
 import router from "./lib/router";
 
