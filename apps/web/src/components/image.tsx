c001af33ea15f12e542efd24bcee738ef2dbd043
Chclt <realchclt@gmail.com>
2025-11-06 22:35:01 +0700

    feat: refactor avatar component and enhance image component with error state

--- apps/web/src/components/image.tsx
+++ apps/web/src/components/image.tsx
@@ -1,18 +1,23 @@
-import type React from "react";
+import React, { useState } from "react";
 
-export default function Image({
-	src,
-	alt,
-	className,
-	...props
-}: React.ImgHTMLAttributes<HTMLImageElement>) {
+export default function Image(
+	props: React.ImgHTMLAttributes<HTMLImageElement>,
+) {
+	const [isError, setIsError] = useState(false);
+	const handleOnImageLoad = () => {
+		setIsError(false);
+	};
+	const handleOnImageError = () => {
+		setIsError(true);
+	};
+
 	return (
 		<img
-			src={src}
-			alt={alt}
 			loading="lazy"
 			referrerPolicy="no-referrer"
-			className={className}
+			onLoad={handleOnImageLoad}
+			onError={handleOnImageError}
+			data-state={isError ? "error" : undefined}
 			{...props}
 		/>
 	);
--- apps/web/src/routes/$accountId/contact/-components/contact-list/contact-item.tsx
+++ apps/web/src/routes/$accountId/contact/-components/contact-list/contact-item.tsx
@@ -1,4 +1,3 @@
-import Image from "@/components/image";
 import { useMiniRouter } from "@/components/mini-router";
 import { Link } from "@tanstack/react-router";
 import { ContactGroupListMiniRouteState } from "./contact-group-list";
@@ -6,6 +5,7 @@ import {
 	ContactListContctGroupItem,
 	ContactListContctItem,
 } from "./use-contact-list";
+import { Avatar } from "@/components/ui/avatar.tsx";
 
 interface ContactItemProps extends React.HTMLAttributes<HTMLLIElement> {
 	accountId: string;
@@ -54,13 +54,7 @@ export default function ContactItem({
 				}}
 			>
 				<div className="shrink-0 py-2.5 ps-5">
-					<div className="w-9 h-9 clothoid-corner-[18.18%]">
-						<Image
-							src={contactItem.photo}
-							alt={contactItem.title}
-							className="w-full h-full object-cover"
-						/>
-					</div>
+					<Avatar src={contactItem.photo} className="w-9 h-9" />
 				</div>
 
 				<div className="flex-grow py-2.5 pe-5 flex flex-col justify-center border-b border-muted">
