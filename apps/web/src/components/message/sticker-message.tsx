c07ce32f9029e57e94151674d5574cb36310b029
Chclt <realchclt@gmail.com>
2025-11-09 21:39:40 +0800

    feat: fallback style for sticker message

--- apps/web/src/components/message/sticker-message.tsx
+++ apps/web/src/components/message/sticker-message.tsx
@@ -2,6 +2,7 @@ import Image from "@/components/image.tsx";
 import MessageInlineWrapper from "@/components/message-inline-wrapper";
 import type { MessageProp } from "@/components/message/message.tsx";
 import type { StickerMessageType } from "@/schema";
+import { cn } from "@/lib/utils.ts";
 
 type StickerMessageProps = MessageProp<StickerMessageType>;
 
@@ -87,13 +88,20 @@ function StickerMessageDefault({
 					: {})}
 				src={message.message_entity.msg.emoji["@_cdnurl"]}
 				alt={"表情"}
-				className={"min-w-11 min-h-11 max-w-32 max-h-32"}
-				style={
-					{
-						// width: message.message_entity.msg.emoji["@_width"],
-						// height: message.message_entity.msg.emoji["@_height"],
-					}
-				}
+				className={cn(
+					"min-w-11 min-h-11 max-w-32 max-h-32",
+					"[&:not([src]),&[data-state='error']]:invisible [&:not([src]),&[data-state='error']]:relative [&:not([src]),&[data-state='error']]:size-16",
+					"[&:not([src]),&[data-state='error']]:after:visible [&:not([src]),&[data-state='error']]:after:absolute [&:not([src]),&[data-state='error']]:after:inset-0 [&:not([src]),&[data-state='error']]:after:p-2 [&:not([src]),&[data-state='error']]:after:flex [&:not([src]),&[data-state='error']]:after:justify-center [&:not([src]),&[data-state='error']]:after:items-center",
+					"[&:not([src]),&[data-state='error']]:after:text-center [&:not([src]),&[data-state='error']]:after:text-xs [&:not([src]),&[data-state='error']]:after:text-muted-foreground/50 [&:not([src]),&[data-state='error']]:after:content-['无法加载的表情'] [&:not([src]),&[data-state='error']]:after:bg-black/5",
+				)}
+				style={{
+					...(Number(message.message_entity.msg.emoji["@_width"])
+						? {
+								width: message.message_entity.msg.emoji["@_width"],
+								height: message.message_entity.msg.emoji["@_height"],
+							}
+						: {}),
+				}}
 			/>
 		</div>
 	);
