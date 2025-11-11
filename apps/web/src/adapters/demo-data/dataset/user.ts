f53a797a43d97d2ad6a509eac24aed966d005793
Chclt <realchclt@gmail.com>
2025-11-11 09:42:45 +0800

    feat: demo data (WIP)

--- apps/web/src/adapters/demo-data/dataset/user.ts
+++ apps/web/src/adapters/demo-data/dataset/user.ts
@@ -40,6 +40,371 @@ export const user_1_contact: ContactType = {
 	is_openim: false,
 };
 
-export const UserList = [user_0, user_1];
+export const user_2: UserType = {
+	id: "wxid_00000000000002",
+	user_id: "wxid_00000000000002",
+	username: "Lili",
+	bio: "一切刚刚好",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
 
-export const ContactList = [user_1_contact];
+export const user_2_contact: ContactType = {
+	id: "wxid_00000000000002",
+	username: "Lili",
+	usernamePinyin: "LILI",
+
+	remark: "李婉清",
+	remarkPinyin: "LIWANQING",
+	remarkPinyinInits: "LWQ",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_3: UserType = {
+	id: "wxid_00000000000003",
+	user_id: "wxid_00000000000003",
+	username: "周周",
+	bio: "代码写到半夜的那种快乐",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_3_contact: ContactType = {
+	id: "wxid_00000000000003",
+	username: "周周",
+	usernamePinyin: "ZHOUZHOU",
+
+	remark: "周凯文",
+	remarkPinyin: "ZHOUKAIWEN",
+	remarkPinyinInits: "ZKW",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_4: UserType = {
+	id: "wxid_00000000000004",
+	user_id: "wxid_00000000000004",
+	username: "小楠",
+	bio: "在路上，总有风景",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_4_contact: ContactType = {
+	id: "wxid_00000000000004",
+	username: "小楠",
+	usernamePinyin: "XIAONAN",
+
+	remark: "王若楠",
+	remarkPinyin: "WANGRUONAN",
+	remarkPinyinInits: "WRN",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_5: UserType = {
+	id: "wxid_00000000000005",
+	user_id: "wxid_00000000000005",
+	username: "宇哥",
+	bio: "静下心来做点事",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_5_contact: ContactType = {
+	id: "wxid_00000000000005",
+	username: "宇哥",
+	usernamePinyin: "YUGE",
+
+	remark: "赵明宇",
+	remarkPinyin: "ZHAOMINGYU",
+	remarkPinyinInits: "ZMY",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_6: UserType = {
+	id: "wxid_00000000000006",
+	user_id: "wxid_00000000000006",
+	username: "然然",
+	bio: "生活要有一点仪式感",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_6_contact: ContactType = {
+	id: "wxid_00000000000006",
+	username: "然然",
+	usernamePinyin: "RANRAN",
+
+	remark: "孙怡然",
+	remarkPinyin: "SUNYIRAN",
+	remarkPinyinInits: "SYR",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_7: UserType = {
+	id: "wxid_00000000000007",
+	user_id: "wxid_00000000000007",
+	username: "浩子",
+	bio: "风会带来新的方向",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_7_contact: ContactType = {
+	id: "wxid_00000000000007",
+	username: "浩子",
+	usernamePinyin: "HAOZI",
+
+	remark: "林志浩",
+	remarkPinyin: "LINZHIHAO",
+	remarkPinyinInits: "LZH",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_8: UserType = {
+	id: "wxid_00000000000008",
+	user_id: "wxid_00000000000008",
+	username: "文文",
+	bio: "不争不抢，慢慢发光",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_8_contact: ContactType = {
+	id: "wxid_00000000000008",
+	username: "文文",
+	usernamePinyin: "WENWEN",
+
+	remark: "徐静文",
+	remarkPinyin: "XUJINGWEN",
+	remarkPinyinInits: "XJW",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_9: UserType = {
+	id: "wxid_00000000000009",
+	user_id: "wxid_00000000000009",
+	username: "小帆",
+	bio: "随遇而安，自在如风",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_9_contact: ContactType = {
+	id: "wxid_00000000000009",
+	username: "小帆",
+	usernamePinyin: "XIAOFAN",
+
+	remark: "郑一帆",
+	remarkPinyin: "ZHENGYIFAN",
+	remarkPinyinInits: "ZYF",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_10: UserType = {
+	id: "wxid_00000000000010",
+	user_id: "wxid_00000000000010",
+	username: "曦宝",
+	bio: "每天都要有一点阳光",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_10_contact: ContactType = {
+	id: "wxid_00000000000010",
+	username: "曦宝",
+	usernamePinyin: "XIBAO",
+
+	remark: "高晨曦",
+	remarkPinyin: "GAOCHENXI",
+	remarkPinyinInits: "GCX",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_11: UserType = {
+	id: "wxid_00000000000011",
+	user_id: "wxid_00000000000011",
+	username: "哲叔",
+	bio: "想得太多不如去做",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_11_contact: ContactType = {
+	id: "wxid_00000000000011",
+	username: "哲叔",
+	usernamePinyin: "ZHESHU",
+
+	remark: "马哲",
+	remarkPinyin: "MAZHE",
+	remarkPinyinInits: "MZ",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_12: UserType = {
+	id: "wxid_00000000000012",
+	user_id: "wxid_00000000000012",
+	username: "OY",
+	bio: "世界不止眼前的代码",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_12_contact: ContactType = {
+	id: "wxid_00000000000012",
+	username: "OY",
+	usernamePinyin: "OY",
+
+	remark: "欧阳靖然",
+	remarkPinyin: "OUYANGJINGRAN",
+	remarkPinyinInits: "OYJR",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_13: UserType = {
+	id: "wxid_00000000000013",
+	user_id: "wxid_00000000000013",
+	username: "rou",
+	bio: "时间会回答所有问题",
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const user_13_contact: ContactType = {
+	id: "wxid_00000000000013",
+	username: "rou",
+	usernamePinyin: "ROU",
+
+	remark: "夏柔",
+	remarkPinyin: "XIAROU",
+	remarkPinyinInits: "XR",
+
+	photo: {
+		origin: "string",
+		thumb: "string",
+	},
+	is_openim: false,
+};
+
+export const UserList = [
+	user_0,
+	user_1,
+	user_2,
+	user_3,
+	user_4,
+	user_5,
+	user_6,
+	user_7,
+	user_8,
+	user_9,
+	user_10,
+	user_11,
+	user_12,
+	user_13,
+];
+
+export const ContactList = [
+	user_1_contact,
+	user_2_contact,
+	user_3_contact,
+	user_4_contact,
+	user_5_contact,
+	user_6_contact,
+	user_7_contact,
+	user_8_contact,
+	user_9_contact,
+	user_10_contact,
+	user_11_contact,
+	user_12_contact,
+	user_13_contact,
+];
