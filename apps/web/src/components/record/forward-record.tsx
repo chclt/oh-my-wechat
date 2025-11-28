8a351231be3ef9d370b46e4887bfd1051f70d188
Chclt <realchclt@gmail.com>
2025-11-29 02:23:13 +0800

    fix: fix style of forward message in forward message

--- apps/web/src/components/record/forward-record.tsx
+++ apps/web/src/components/record/forward-record.tsx
@@ -64,7 +64,7 @@ export default function ForwardMessageRecord({
 					<div
 						className={forwardMessageRecordVariants({
 							variant,
-							direction: message.direction,
+							direction: MessageDirection.incoming,
 							className,
 						})}
 						style={{
