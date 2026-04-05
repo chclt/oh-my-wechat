d3c44da5f1bee671377c8b26abc566e299149234
Chclt <realchclt@gmail.com>
2026-04-05 11:59:39 +0800

    feat: chat media carousel

--- packages/adapter-ios-backup/src/controllers/message.ts
+++ packages/adapter-ios-backup/src/controllers/message.ts
@@ -604,7 +604,7 @@ export async function all(...inputs: AllInput): AllOutput {
 									? and(...baseRightQueryWhereSegmentConditions)
 									: undefined;
 
-							const baseLeftQuery = database
+							const baseLeftSubquery = database
 								.select(chatTableSelect(chatTable))
 								.from(chatTable)
 								.where(baseLeftQueryWhereSegment)
@@ -612,7 +612,7 @@ export async function all(...inputs: AllInput): AllOutput {
 								.limit(query_limit)
 								.as("baseLeftQuery");
 
-							const baseRightQuery = database
+							const baseRightSubquery = database
 								.select(chatTableSelect(chatTable))
 								.from(chatTable)
 								.where(baseRightQueryWhereSegment)
@@ -620,10 +620,10 @@ export async function all(...inputs: AllInput): AllOutput {
 								.limit(query_limit)
 								.as("baseRightQuery");
 
-							// @ts-ignore
-							const baseQuery = unionAll(baseLeftQuery, baseRightQuery).as(
-								"baseQuery",
-							);
+							const baseQuery = unionAll(
+								database.select().from(baseLeftSubquery),
+								database.select().from(baseRightSubquery),
+							).as("baseQuery");
 
 							const query = database
 								.select()
