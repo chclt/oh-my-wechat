61d7226f491fe9a694f07862b9d09f3a54ddc010
Chclt <realchclt@gmail.com>
2026-06-07 10:12:57 +0800

    fix: rename wxgf function name (edit)

--- packages/adapter-ios-backup/src/utils/wxgf/index.ts
+++ packages/adapter-ios-backup/src/utils/wxgf/index.ts
@@ -1,3 +1,3 @@
-export async function convertWxgfToJpg(_wxgf: Uint8Array): Promise<string> {
+export async function convertWxgfToImage(_wxgf: Uint8Array): Promise<string> {
 	throw new Error("Unsupported WXGF image.");
 }
